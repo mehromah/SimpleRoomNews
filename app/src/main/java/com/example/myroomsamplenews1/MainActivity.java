0bb3dd4560e9a4c38387904de3cbed1ce1ff04ab
mehromah <m.dehdar2018@gmail.com>
2019-03-05 03:15:52 +0330

    mj commit

--- app/src/main/java/com/example/myroomsamplenews1/MainActivity.java
+++ app/src/main/java/com/example/myroomsamplenews1/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.myroomsamplenews1;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,21 +8,18 @@ import android.util.Log;
 
 import com.example.myroomsamplenews1.data.News;
 
-
 import org.reactivestreams.Subscription;
 
 import java.util.List;
 
-
 import io.reactivex.FlowableSubscriber;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    public MainViewModel viewModel;
-    public NewsAdapter newsAdapter;
+    public  MainViewModel       viewModel;
+    public  NewsAdapter         newsAdapter;
     public RecyclerView newsRecyclerView;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
@@ -34,43 +29,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewModel = new MainViewModel(this);
         newsRecyclerView = findViewById(R.id.rv_home_newsList);
-        newsRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        newsRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        newsRecyclerView.setNestedScrollingEnabled(false);
         newsAdapter = new NewsAdapter();
         newsRecyclerView.setAdapter(newsAdapter);
-
         viewModel.getNewsList().subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new FlowableSubscriber<List<News>>() {
-                    @Override
-                    public void onSubscribe(Subscription s) {
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new FlowableSubscriber<List<News>>() {
+                @Override
+                public void onSubscribe(Subscription s) {
+                    s.request(Long.MAX_VALUE);
+                    Log.e("MainActivity", "onSubscribe: " + s.toString());
+                }
 
-                    }
+                @Override
+                public void onNext(List<News> news) {
+                    newsAdapter.setNewsList(news);
+                    Log.e("MainActivity", "onNext: " + news.size());
+                }
 
-                    @Override
-                    public void onNext(List<News> news) {
-                        newsAdapter.setNewsList(news);
+                @Override
+                public void onError(Throwable t) {
+                    Log.e("MainActivity", "onError: " + t.toString());
+                }
 
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        Log.e("MainActivity", "onError: " + t.toString());
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
-//
-
-        newsRecyclerView.setNestedScrollingEnabled(false);
-
-
+                @Override
+                public void onComplete() {
+                    Log.e("MainActivity", "onComplete");
+                }
+            });
     }
-
-
-
-
 }
